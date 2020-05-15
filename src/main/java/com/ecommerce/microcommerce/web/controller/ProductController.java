333da322f9942b2ec5919a607437ca4a5500fbb6
Thierno Samb <thierno.samb@chakamobile.com>
2020-05-15 15:45:50 +0000

    Partie 01 OK

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -16,7 +16,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Api( description="API pour es opérations CRUD sur les produits.")
@@ -103,6 +105,13 @@ public class ProductController {
         return productDao.chercherUnProduitCher(400);
     }
 
-
+    @GetMapping("/AdminProduits")
+    public Map<String,Integer> calculerMargeProduit(){
+        Map<String,Integer> margePro = new HashMap<>();
+        productDao.findAll().forEach(product->{
+            margePro.put(product.toString() , Math.abs(product.getPrix()-product.getPrixAchat()));
+        });
+        return margePro;
+    }
 
 }
